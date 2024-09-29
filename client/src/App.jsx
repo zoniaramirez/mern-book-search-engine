62954c77e79a0404f1d6fad94e9009b56e18dc0d
Zonia Ramirez <jslnramirez@gmail.com>
2024-09-28 20:37:51 -0400

    updated App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,43 @@
 import './App.css';
 import { Outlet } from 'react-router-dom';
+import { 
+  ApolloClient, 
+  InMemoryCache, 
+  ApolloProvider, 
+  createHttpLink 
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
 
 import Navbar from './components/Navbar';
 
+// Create an HTTP link to the GraphQL server
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+// Set up the authentication link to include the token in the headers
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+// Create the Apollo Client
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
+
 function App() {
   return (
-    <>
+    <ApolloProvider client={client}>
       <Navbar />
       <Outlet />
-    </>
+    </ApolloProvider>
   );
 }
 
